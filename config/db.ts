98a5a0669495c7ac3eb7217acaf03f9fc7d41553
ivan <ivan.correa24@yahoo.com>
2023-07-20 14:07:49 -0300

    feat:updating-dockerfile

---
+++ config/db.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const URL = "mongodb://localhost/fast-delivery-back";
+
+mongoose
+  .connect(URL)
+  .then(() => console.log("DB is connected"))
+  .catch((error) => console.log(error));
+
+export default mongoose;
