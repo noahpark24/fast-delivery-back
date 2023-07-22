5c3350f166db399b33ed5c55405bfe5b3e427177
ivan <ivan.correa24@yahoo.com>
2023-07-21 23:18:59 -0300

    feat:updating-user-model v1.0.1

--- models/User.ts
+++ models/User.ts
@@ -1,63 +1,76 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import validator from 'validator'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import validator from "validator";
 
 const User = new mongoose.Schema({
-	username: {
-		type: String,
-		required: [true, 'enter a username'],
-		unique: true,
-	},
-	email: {
-		type: String,
-		required: [true, 'enter an email'],
-		unique: true,
-		validate: [validator.isEmail, 'enter an valid email'],
-	},
-	password: {
-		type: String,
-		required: [true, 'enter a password'],
-		validate: [
-			(str) => {
-				validator.isStrongPassword(str, {
-					minLength: 8,
-					minUppercase: 1,
-					minSymbols: 0,
-					minNumbers: 0,
-					returnScore: false,
-				})
-			},
-			'The password must have 8 characters and 1 capital letter',
-		],
-	},
-	salt: {
-		type: String,
-	},
-	is_deleted: {
-		type: Boolean,
-		default: false,
-	},
-	is_admin: {
-		type: Boolean,
-		default: false,
-	},
-})
+  username: {
+    type: String,
+    required: [true, "enter a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "enter an email"],
+    unique: true,
+    validate: [validator.isEmail, "enter an valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "enter a password"],
+    validate: [
+      (str) => {
+        validator.isStrongPassword(str, {
+          minLength: 8,
+          minUppercase: 1,
+          minSymbols: 0,
+          minNumbers: 0,
+          returnScore: false,
+        });
+      },
+      "The password must have 8 characters and 1 capital letter",
+    ],
+  },
+  salt: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: [true, "enter yor name"],
+  },
+  last_name: {
+    type: String,
+    required: [true, "enter your lastname"],
+  },
+  profile_img: {
+    type: String,
+    default:
+      "https://w7.pngwing.com/pngs/722/101/png-transparent-computer-icons-user-profile-circle-abstract-miscellaneous-rim-account.png",
+  },
+  is_deleted: {
+    type: Boolean,
+    default: false,
+  },
+  is_admin: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-User.pre('save', function (next) {
-	if (!this.isModified('password')) {
-		return next()
-	}
-	const salt = bcrypt.genSaltSync(8)
-	this.salt = salt
-	return bcrypt.hash(this.password, this.salt).then((hash) => {
-		this.password = hash
-	})
-})
+User.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = bcrypt.genSaltSync(8);
+  this.salt = salt;
+  return bcrypt.hash(this.password, this.salt).then((hash) => {
+    this.password = hash;
+  });
+});
 
 User.methods.validatePassword = async function (password) {
-	const hash = await bcrypt.hash(password, this.salt)
-	const checking = hash === this.password
-	return checking
-}
+  const hash = await bcrypt.hash(password, this.salt);
+  const checking = hash === this.password;
+  return checking;
+};
 
-module.exports = mongoose.model('User', User)
+module.exports = mongoose.model("User", User);
