9cedfb20c54d9399768ee12f9bf92233d4e64ce1
Jose Moya <jose_moya@live.com.ar>
2023-08-08 17:41:19 -0300

    feat: signup first stages

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import routes from "./routes/index";
 import connectDB from "./config/db";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const server = express();
 
@@ -11,6 +12,13 @@ server.use(bodyParser.json());
 server.use(express.json());
 server.use(cookieParser());
 server.use("/api", routes);
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 connectDB();
 server.listen(3001, "0.0.0.0", () => {
