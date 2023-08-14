afbda0f56d0907fa8a47450e2afe8c70544be65a
ivan <ivan.correa24@yahoo.com>
2023-08-14 11:46:16 -0300

    feat: adding validated hola route

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,10 @@
-import user from "./user.routes";
-import packages from "./package.route";
-import express from "express";
+import user from './user.routes'
+import packages from './package.route'
+import express from 'express'
 
-const router = express();
+const router = express()
 
-router.use("/users", user);
-router.use("/packages", packages);
+router.use('/users', user)
+router.use('/packages', packages)
 
-export default router;
+export default router
--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import { hola, login, logout, signup } from "../controllers/user.controller";
-import validateUser from "../middlewares/auth";
+import express from 'express'
+import { hola, login, logout, signup } from '../controllers/user.controller'
+import validateUser from '../middlewares/auth'
 
-const router = express();
+const router = express()
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/hola", validateUser, hola);
-router.post("/logout", validateUser, logout);
+router.post('/signup', signup)
+router.post('/login', login)
+router.post('/hola', validateUser, hola)
+router.post('/logout', validateUser, logout)
 
-export default router;
+export default router
