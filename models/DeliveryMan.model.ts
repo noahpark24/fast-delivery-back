c4a6afa0acca9ae93ce49fe70c8ff65c4fe20f0a
ivan <ivan.correa24@yahoo.com>
2023-08-28 15:56:37 -0300

    feat: updating usermodel deliveryman model

--- models/DeliveryMan.model.ts
+++ models/DeliveryMan.model.ts
@@ -1,31 +1,31 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 const DeliveryMan = new Schema({
-  current_deliveries: {
-    type: Number,
-    default: 0,
-    max: 10,
-  },
-  active: {
-    type: Boolean,
-    default: false,
-  },
-  drinked_alcohol: {
-    type: Boolean,
-    default: false,
-  },
-  taked_drugs: {
-    type: Boolean,
-    default: false,
-  },
-  sleeped_well: {
-    type: Boolean,
-    default: true,
-  },
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  packages: [{ type: Schema.Types.ObjectId, ref: "Package" }],
-});
+	current_deliveries: {
+		type: Number,
+		default: 0,
+		max: 10,
+	},
+	active: {
+		type: Boolean,
+		default: false,
+	},
+	drinked_alcohol: {
+		type: Boolean,
+		default: false,
+	},
+	taked_drugs: {
+		type: Boolean,
+		default: false,
+	},
+	sleeped_well: {
+		type: Boolean,
+		default: true,
+	},
+	user: { type: Schema.Types.ObjectId, ref: 'User' },
+	packages: [{ type: Schema.Types.ObjectId, ref: 'Package' }],
+})
 
-const DeliveryManModel = model("Deliveryman", DeliveryMan);
+const DeliveryManModel = model('Deliveryman', DeliveryMan)
 
-export default DeliveryManModel;
+export default DeliveryManModel
--- services/user.service.ts
+++ services/user.service.ts
@@ -1,46 +1,46 @@
-import { User as UserModel } from "../models";
-import { DeliveryMan } from "../models";
-import { UserInterface } from "../interfaces/user.interfaces";
-import { UserWithPasswordValidation } from "../interfaces/user.interfaces";
+import { User as UserModel } from '../models'
+import { DeliveryMan } from '../models'
+import { UserInterface } from '../interfaces/user.interfaces'
+import { UserWithPasswordValidation } from '../interfaces/user.interfaces'
 
 export default class User_Services {
-  async createUser(userData: UserInterface) {
-    try {
-      const createdUser = await UserModel.create(userData);
-      await createdUser.save();
+	async createUser(userData: UserInterface) {
+		try {
+			const createdUser = await UserModel.create(userData)
+			await createdUser.save()
 
-      if (!createdUser.is_admin) {
-        const deliveryMan = await new DeliveryMan({
-          user: createdUser,
-        });
-        await deliveryMan.populate("user");
-        await deliveryMan.save();
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
+			if (!createdUser.is_admin) {
+				const deliveryMan = await new DeliveryMan({
+					user: createdUser,
+				})
+				await deliveryMan.populate('user')
+				await deliveryMan.save()
+			}
+		} catch (error) {
+			throw error
+		}
+	}
 
-  async findByUserEmail(email: string) {
-    try {
-      const user: UserWithPasswordValidation | null = await UserModel.findOne({
-        email,
-      });
-      return user;
-    } catch (error) {
-      throw error;
-    }
-  }
-  async validateUserPassword(
-    user: UserWithPasswordValidation,
-    password: string
-  ) {
-    try {
-      const isValid = await user.validatePassword(password);
+	async findByUserEmail(email: string) {
+		try {
+			const user: UserWithPasswordValidation | null = await UserModel.findOne({
+				email,
+			})
+			return user
+		} catch (error) {
+			throw error
+		}
+	}
+	async validateUserPassword(
+		user: UserWithPasswordValidation,
+		password: string
+	) {
+		try {
+			const isValid = await user.validatePassword(password)
 
-      return isValid;
-    } catch (error) {
-      throw error;
-    }
-  }
+			return isValid
+		} catch (error) {
+			throw error
+		}
+	}
 }
