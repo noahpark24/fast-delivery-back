2a394970f59d1ec09893d79b60be98e7d9a65890
ivan <ivan.correa24@yahoo.com>
2023-09-04 17:39:27 -0300

    fix: db.ts URL fixed

--- config/db.ts
+++ config/db.ts
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
-const URL = process.env.URL!
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const URL: string = process.env.URL || '';
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(URL)
-		console.log('DB is connected')
-	} catch (error) {
-		console.error('Error connecting to database:', error)
-	}
-}
-export default connectDB
+  try {
+    await mongoose.connect(URL);
+    console.log('DB is connected');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+  }
+};
+export default connectDB;
