be432b7914ade06432b3405acf4bf8bb80ee4b21
leonstefano03 <leonstefano03@gmail.com>
2023-09-11 18:21:15 -0300

    feat/cleaning-services

--- services/packages.services.ts
+++ services/packages.services.ts
@@ -14,59 +14,32 @@ export class PackagesServices {
 	}
 
 	async getPackages() {
-		try {
 			const allPackages = await Package.find()
 			return allPackages
-		} catch (error) {
-			console.log('Error getting packages: ', error)
-			throw error
-		}
 	}
 
 	async getPackage(id: string) {
-		try {
 			const onePackage = await Package.findById(id)
 			return onePackage
-		} catch (error) {
-			console.log('Error getting package: ', error)
-			throw error
-		}
 	}
 
 	async createPackage(data: PackageInterface) {
-		try {
 			const newPackage = new Package(data);
 			await newPackage.save();
 			return newPackage;
-		} catch (error) {
-			console.log('Error creating package: ', error);
-			throw error;
-		}
 	}
 	
 	async deletePackage(id: string) {
-		try {
 			const deletedPackage = await Package.findByIdAndRemove(id, {
 				select: '_id',
 			})
-			if (deletedPackage) {
-				console.log('Package removed')
-			} else {
+			if (!deletedPackage) {
 				console.log('Package not found')
-			}
-		} catch (error) {
-			console.log('Error removing package: ', error)
-			throw error
 		}
 	}
 
 	async editPackage(id: string, updatedData: PackageInterface) {
-		try {
 			const updatedPackage = await Package.findByIdAndUpdate(id, updatedData)
 			return updatedPackage
-		} catch (error) {
-			console.log('Error editing package: ', error)
-			throw error
-		}
 	}
 }
