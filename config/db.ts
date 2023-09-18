986ab899f4c061e178887f517ea5dbef346860a7
ivan <ivan.correa24@yahoo.com>
2023-09-17 22:09:51 -0300

    feat: adding docker-compose back

--- config/db.ts
+++ config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'; //el dotenv es para la variable del .env de la url
+import dotenv from 'dotenv';
 dotenv.config();
 const URL: string = process.env.URL || '';
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(URL)
-		console.log('DB is connected')
-	} catch (error) {
-		console.error('Error connecting to database:', error)
-	}
-}
-export default connectDB
+  try {
+    await mongoose.connect(URL);
+    console.log('DB is connected');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+  }
+};
+export default connectDB;
